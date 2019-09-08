5f3b5b8e9deaad7b51ce5b2fd07404837e971fe4
Roman <P0M3E3@gmail.com>
2019-09-08 17:31:50 +0200

    10

--- virus.js
+++ virus.js
@@ -6,25 +6,25 @@ let dir='right';
 setInterval(()=>{
 switch (dir){
 case 'right':
-win.moveBy(5,0);
+win.moveBy(10,0);
 xp+=5;
 if (xp>=100)
 dir='down';
 break;
 case 'down':
-win.moveBy(0,5);
+win.moveBy(0,10);
 yp+=5;
 if (yp>=100)
 dir='left';
 break;
 case 'left':
-win.moveBy(-5,0);
+win.moveBy(-10,0);
 xp-=5;
 if (xp<=0)
 dir='up';
 break;
 case 'up':
-win.moveBy(0,-5);
+win.moveBy(0,-10);
 yp-=5;
 if(yp<=0)
 dir='right';
