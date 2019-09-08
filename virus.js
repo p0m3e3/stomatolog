1c92b1d41bd2e59c64abc05ba023c5db6af74dd8
medvosa <38169798+medvosa@users.noreply.github.com>
2019-09-08 17:12:34 +0300

    Create virus.js

---
+++ virus.js
@@ -0,0 +1,34 @@
+{
+win=open('forms.html','example','width=100,height=100')
+let xp=0;
+let yp=0;
+let dir='right';
+setInterval(()=>{
+switch (dir){
+case 'right':
+win.moveBy(5,0);
+xp+=5;
+if (xp>=100)
+dir='down';
+break;
+case 'down':
+win.moveBy(0,5);
+yp+=5;
+if (yp>=100)
+dir='left';
+break;
+case 'left':
+win.moveBy(-5,0);
+xp-=5;
+if (xp<=0)
+dir='up';
+break;
+case 'up':
+win.moveBy(0,-5);
+yp-=5;
+if(yp<=0)
+dir='right';
+break;
+}
+},500)
+}
